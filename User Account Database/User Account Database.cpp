8a5a86598912592c36cebeb3ece5d6b037255a09
Ehsan Ali <ehsan27ali@gmail.com>
2020-09-26 04:55:32 -0500

    Main menu created

--- User Account Database/User Account Database.cpp
+++ User Account Database/User Account Database.cpp
@@ -1,21 +1,38 @@
 // User Account Database.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <stdlib.h>
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+//protocols
+int mainMenu();
+
+int main() {
+
+	int userInput = mainMenu();
+
+}
+
+int mainMenu() {
+
+	char userInput;
+
+	do {
+		cout << "\nUser Account Database" << endl;
+		cout << "=====================" << endl;
+		cout << "1)Create New Account";
+		cout << "\n2)Sign in" << endl;
+		cout << "User input: ";
+		cin >> userInput;
+
+		if (userInput != '1' && userInput != '2')
+			cout << "\nERROR. Invalid user input." << endl;
+	} while (userInput != '1' && userInput != '2');
+
+	int userInput_int = (int)userInput;
+	return userInput_int;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
